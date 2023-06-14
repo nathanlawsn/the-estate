43fc519e42894f052da3659ae7760d051069bfa2
Nathan Lawson <nathan@cargocreative.co.uk>
2023-06-14 10:00:08 +0100

    Fix issue with images on build

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,6 +2,6 @@ import ReactPlayer from 'react-player/youtube'
 
 export default function Video() {
     return (
-        <ReactPlayer class="aspect-video" width="100%" height="100%" url='https://www.youtube.com/watch?v=_kBMhQ7_AwY' />
+        <ReactPlayer className="aspect-video" width="100%" height="100%" url='https://www.youtube.com/watch?v=_kBMhQ7_AwY' />
     );
 }
