a7eccd81eadb815347cbfd0c7f130a2dc8aa189b
Nathan Lawson <nathan@cargocreative.co.uk>
2023-07-11 14:32:22 +0100

    Fix JSX error

--- src/components/react/PostsCategory.jsx
+++ src/components/react/PostsCategory.jsx
@@ -26,14 +26,14 @@ export default function PostsCategory({ posts }) {
             </div>
             
             <button className="bg-orange-400 text-white flex items-center gap-5 sm:gap-[4.75rem] px-5 md:px-11 py-3 min-h-[5rem] sm:min-h-[6.25rem] w-full hover:bg-primary-purple transition-colors duration-500 ease-in-out group text-center justify-center" onClick={loadMoreItems}>
-                <span class="text-2xl sm:text-[2.125rem] ml-auto">Load more articles</span>
-                <span class="block w-6 h-6 sm:w-8 sm:h-8 group-hover:rotate-45 duration-500 ease-in-out ml-auto">
+                <span className="text-2xl sm:text-[2.125rem] ml-auto">Load more articles</span>
+                <span className="block w-6 h-6 sm:w-8 sm:h-8 group-hover:rotate-45 duration-500 ease-in-out ml-auto">
                     <svg viewBox="0 0 17.978 17.978" astro-icon="arrow">
                         <g fill="none"
                             stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             ><path
                                 d="m1.414 16.564 15.15-15.15"
                                 data-name="Line 1"></path><path
