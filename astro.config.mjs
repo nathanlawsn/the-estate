c1b0821d7c3ab63ca67bb05ab32401d03a1e17fa
Nathan Lawson <nathan@cargocreative.co.uk>
2023-07-27 17:30:19 +0100

    Astro Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,16 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import prefetch from "@astrojs/prefetch";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  }), compress()],
   experimental: {
     viewTransitions: true
   },
   build: {
-    inlineStylesheets: 'auto'
   }
 });
