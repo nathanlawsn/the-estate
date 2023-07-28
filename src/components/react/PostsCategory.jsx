a595e7b134f93515b8a13442cdd3f9d3e5594967
nathanlawson <nathan@cargocreative.co.uk>
2023-07-28 12:32:29 +0100

    Hide load more button when no posts to show

--- src/components/react/PostsCategory.jsx
+++ src/components/react/PostsCategory.jsx
@@ -9,6 +9,8 @@ export default function PostsCategory({ posts }) {
     setDisplayedItems(prevCount => prevCount + 9);
   };
 
+  const hasMorePostsToShow = displayedItems < posts.length;
+
     return (
         <>
         {posts.length > 0 ? (
@@ -26,25 +28,27 @@ export default function PostsCategory({ posts }) {
                         />
                     ))}
                 </div>
-                
-                <button className="bg-primary-orange text-white flex items-center gap-5 sm:gap-[4.75rem] px-5 md:px-11 py-3 min-h-[5rem] sm:min-h-[6.25rem] w-full hover:bg-primary-purple transition-colors duration-500 ease-in-out group text-center justify-center" onClick={loadMoreItems}>
-                    <span className="text-2xl sm:text-[2.125rem] ml-auto">Load more articles</span>
-                    <span className="block w-6 h-6 sm:w-8 sm:h-8 group-hover:rotate-45 duration-500 ease-in-out ml-auto">
-                        <svg viewBox="0 0 17.978 17.978" astro-icon="arrow">
-                            <g fill="none"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                ><path
-                                    d="m1.414 16.564 15.15-15.15"
-                                    data-name="Line 1"></path><path
-                                    d="M1.414 1.414h15.18v15.18"
-                                    data-name="Path 1"></path>
-                            </g>
-                        </svg>
-                    </span>
-                </button>
+
+                {hasMorePostsToShow && ( 
+                    <button className="bg-primary-orange text-white flex items-center gap-5 sm:gap-[4.75rem] px-5 md:px-11 py-3 min-h-[5rem] sm:min-h-[6.25rem] w-full hover:bg-primary-purple transition-colors duration-500 ease-in-out group text-center justify-center" onClick={loadMoreItems}>
+                        <span className="text-2xl sm:text-[2.125rem] ml-auto">Load more articles</span>
+                        <span className="block w-6 h-6 sm:w-8 sm:h-8 group-hover:rotate-45 duration-500 ease-in-out ml-auto">
+                            <svg viewBox="0 0 17.978 17.978" astro-icon="arrow">
+                                <g fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    ><path
+                                        d="m1.414 16.564 15.15-15.15"
+                                        data-name="Line 1"></path><path
+                                        d="M1.414 1.414h15.18v15.18"
+                                        data-name="Path 1"></path>
+                                </g>
+                            </svg>
+                        </span>
+                    </button>
+                )}
             </div>
             ) : (
             <div>
